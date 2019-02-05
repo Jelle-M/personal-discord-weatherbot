f5cf76f059cadf1c98e6b6cb14e673a417f075d6
MoskiMBA <MoskiMBA@hotmail.com>
2019-02-05 22:25:24 +0100

    Init

---
+++ tests/test_weatherbot.py
@@ -0,0 +1,7 @@
+"""Testing weatherbot"""
+from weatherbot import __version__
+
+
+def test_version():
+    """Test version"""
+    assert __version__ == '0.1.0'
---
+++ weather_bot/hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Print Hello, World!."""
+
+
+def main():
+    """Print Hello world."""
+    return 'hello world!'
+
+
+if __name__ == '__main__':
+    main()
