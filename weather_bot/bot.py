1562740b3fab3a1381fe6dc54ba1cde0ff6066f1
MoskiMBA <MoskiMBA@hotmail.com>
2019-02-06 18:23:58 +0100

    Basic functional bot that runs locally
    Allows you to say "!hello" and the bot will respond

---
+++ weather_bot/bot.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""Discord bot."""
+import argparse
+import logging as log
+
+import discord
+
+from weather_bot.config import token
+
+TOKEN = token.DISCORD_BOT_TOKEN
+client = discord.Client()
+client.run(TOKEN)
+
+
+@client.event
+async def on_message(message):
+    """We do not want the bot to reply to itself."""
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+
+@client.event
+async def on_ready():
+    """Log some info when ready."""
+    log.debug('Logged in as')
+    log.debug(client.user.name)
+    log.debug(client.user.id)
+    log.debug('------')
+
+
+def parse_args():
+    """Parse arguments."""
+    parser = argparse.parser()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='Show debug messages',
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.verbose:
+        log.basicConfig(filename='weather_bot.log', level=log.DEBUG)
---
+++ weather_bot/config/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains some configuration files."""
