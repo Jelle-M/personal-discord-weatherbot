1945f7c23e9eefbedbef4ca447ee06932e199212
MoskiMBA <MoskiMBA@hotmail.com>
2019-02-11 15:15:56 +0100

    Comply with styleguide

--- weather_bot/config/token.py
+++ weather_bot/config/token.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from os import import getenv
 """This file contains the bot token."""
-DISCORD_BOT_TOKEN = getenv('DISCORD_BOT_TOKEN', 'invalid token')
-DARK_SKY_TOKEN = getenv('DARK_SKY_TOKEN', 'invalid token')
+from os import getenv
+
+DISCORD_BOT_TOKEN = getenv('DISCORD_BOT_TOKEN')
+DARK_SKY_TOKEN = getenv('DARK_SKY_TOKEN')
