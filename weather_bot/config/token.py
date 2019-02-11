5f16532ce6431ed6f24ff71ef086d619d153f079
MoskiMBA <MoskiMBA@hotmail.com>
2019-02-11 15:10:16 +0100

    Load token from Heroku env

---
+++ weather_bot/config/token.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from os import import getenv
+"""This file contains the bot token."""
+DISCORD_BOT_TOKEN = getenv('DISCORD_BOT_TOKEN', 'invalid token')
+DARK_SKY_TOKEN = getenv('DARK_SKY_TOKEN', 'invalid token')
