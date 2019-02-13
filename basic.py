d7ee5ddb4e8032b3a9601ca0d5a986d29ea8c817
MOSKI <46423958+MoskiMBA@users.noreply.github.com>
2019-02-13 16:42:52 +0100

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import discord
 import os
 
+from keep_alive import keep_alive
 
 client = discord.Client()
 
@@ -15,5 +16,6 @@ async def on_message(message):
         await client.send_message(message.channel, message.content[::-1])
 
 
-token = os.environ.get("DISCORD_BOT_TOKEN")
+keep_alive()
+token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
