41fb5a9fa914c12b6a0c42dc5a389e995967cda6
MoskiMBA <MoskiMBA@hotmail.com>
2019-02-13 17:23:30 +0100

    Revert "Expose API Token"
    This reverts commit b3e0e99d73617c12c2076502dd1e0e67c5c23232.

--- weather_bot/config/token.py
+++ weather_bot/config/token.py
@@ -3,5 +3,5 @@
 """This file contains the bot token."""
 from os import getenv
 
-DISCORD_BOT_TOKEN = 'NTQyNzI2NDg1MjQxNDk1NTUy.D0XUog.U-ePB0Ui1oO8ryFJhX9Ol196ANs'
+DISCORD_BOT_TOKEN = getenv('DISCORD_BOT_TOKEN')
 DARK_SKY_TOKEN = getenv('DARK_SKY_TOKEN')
